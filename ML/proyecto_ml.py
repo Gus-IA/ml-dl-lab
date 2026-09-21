38d80f9ac0dd29599951a73a36fb56642fb00fbe
Gus-IA <garciagus1992@gmail.com>
2026-09-21 09:35:02 +0200

    Preparación de datos

--- ML/proyecto_ml.py
+++ ML/proyecto_ml.py
@@ -115,3 +115,127 @@ plt.show()
 # buscando correlaciones
 corr_matrix = data.drop(columns=["ocean_proximity"]).corr()
 corr_matrix["median_house_value"].sort_values(ascending=False)
+
+
+# feature engineering
+
+data["rooms_per_household"] = data["total_rooms"] / data["households"]
+data["bedrooms_per_room"] = data["total_bedrooms"] / data["total_rooms"]
+data["population_per_household"] = data["population"] / data["households"]
+
+# buscando correlaciones
+corr_matrix = data.drop(columns=["ocean_proximity"]).corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+
+# preparación datos
+
+data = pd.read_csv("housing_train.csv")
+print(data.head())
+
+data, labels = (
+    data.drop(["median_house_value"], axis=1),
+    data["median_house_value"].copy(),
+)
+print(data.head())
+
+print(labels.head())
+
+# limpieza datos
+
+print(data[data.isnull().any(axis=1)])
+
+data.dropna(subset=["total_bedrooms"])
+
+
+# separamos variables numéricas y categóricas
+
+data_num = data.drop(["ocean_proximity"], axis=1)
+print(data_num.head())
+
+data_cat = data[["ocean_proximity"]]
+print(data_cat.head())
+
+
+from sklearn.impute import SimpleImputer
+
+# rellenamos los missing values con la mediana
+imputer = SimpleImputer(strategy="median")
+imputer.fit(data_num)
+imputer.statistics_
+
+print(data_num.median().values)
+
+# mostramos los datos ya rellenados
+X = imputer.transform(data_num)
+print(X)
+
+
+from sklearn.preprocessing import OrdinalEncoder
+
+# transformar datos a valores numéricos
+ordinal_encoder = OrdinalEncoder()
+data_cat_encoded = ordinal_encoder.fit_transform(data_cat)
+print(data_cat_encoded[:10])
+
+print(ordinal_encoder.categories_)
+
+
+# flujo de transformación de datos
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# column index
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True):  # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[
+                X, rooms_per_household, population_per_household, bedrooms_per_room
+            ]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+data_extra_attribs = attr_adder.transform(data.values)
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+# creamos la pipeline aplicando el imputer y el escalador
+num_pipeline = Pipeline(
+    [
+        ("imputer", SimpleImputer(strategy="median")),
+        # ('attribs_adder', CombinedAttributesAdder()),
+        ("std_scaler", StandardScaler()),
+    ]
+)
+
+data_num_tr = num_pipeline.fit_transform(data_num)
+
+from sklearn.compose import ColumnTransformer
+
+# creamos otra pipeline para transformar los valores numéricos
+num_attribs = list(data_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer(
+    [
+        ("num", num_pipeline, num_attribs),
+        # ("cat", OneHotEncoder(), cat_attribs),
+    ]
+)
+
+data_prepared = full_pipeline.fit_transform(data)
